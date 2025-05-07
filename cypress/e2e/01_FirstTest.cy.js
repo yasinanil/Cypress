fe882451c47282365aca2f6a52f6f5bda2e84240
ANIL <anl@example.com>
2025-05-07 21:12:02 +0300

    Writing and Running Tests

---
+++ cypress/e2e/01_FirstTest.cy.js
@@ -0,0 +1,14 @@
+// <reference types="cypress" />
+
+describe('testsuit', function(){
+
+    it('test1', function(){
+        cy.visit("https://opensource-demo.orangehrmlive.com/");
+        cy.title().should("eq","OrangeHRM");
+    })
+
+    it('test2', ()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/");
+        cy.title().should("eq","OrangeHRMXYZ");
+    })
+});
