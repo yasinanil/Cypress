beaab08b60a95f97fd42224641f72a833b3301d4
ANIL <anl@example.com>
2025-05-19 17:36:19 +0300

    HTML Reports (Mochawsome)

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video: true,
+  reporter: 'cypress-mochawesome-reporter',
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
       on('after:spec', (spec, results) => {
         if (results && results.video && results.stats.failures === 0) {
           const fs = require('fs')
